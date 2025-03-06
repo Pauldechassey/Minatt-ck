a385b61f62aeeda53b31d3ff632ea5e8336a062a
Antilooops <louissavignac333@gmail.com>
2025-03-06 20:40:29 +0100

    annoter partie importante

--- frontend/mainwindow.py
+++ frontend/mainwindow.py
@@ -79,7 +79,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Main Window")
         self.resize(800, 600)
-        self.central_widget = QStackedWidget()
+        self.central_widget = QStackedWidget()  # SUPER IMPORTANT
         self.setCentralWidget(self.central_widget)
 
         self.login_widget = LoginWidget(self.central_widget)
