fb7a606ba11b6893ddef3765cabe26eea10fd6a0
pdechassey <paul.debureteldechasse@epfedu.fr>
2025-03-13 13:57:59 +0100

    Ajout : Reflected XSS & fichier Attaque

---
+++ attaque/Attaque.py
@@ -0,0 +1,46 @@
+from Attaque_sqli import Attaque_sqli
+from Attaque_xss import Attaque_xss
+
+class Attaque:
+    def __init__(self):
+        self.resultats_attaques = {
+            "sqli": [],
+            "xss": []
+        }
+        
+    def attaque_sqli(self, url):
+        scanner = Attaque_sqli()
+        resultats = scanner.run_sqli(url)
+        return resultats
+    def attaque_xss(self, url):
+        scanner = Attaque_xss()
+        resultats = scanner.run_xss(url)
+        return resultats
+    
+    def reinitialiser(self):
+        self.resultats_attaques = {"sqli": []}
+        return True
+
+    def get_resultats(self):
+        return self.resultats_attaques
+
+
+if __name__ == "__main__":
+    """
+    pour lancer le serveur :> python test_state/test_vuln.py
+    -> http://127.0.0.1:5000
+
+    """
+    urls_test = [
+        "http://127.0.0.1:5000",
+        "http://127.0.0.1:5000/xss",
+        "http://127.0.0.1:5000/sqli",
+        "http://127.0.0.1:5000/login"
+    ]
+    attaque = Attaque()
+    for url in urls_test:
+        attaque.resultats_attaques["sqli"].append(attaque.attaque_sqli(url))
+        attaque.resultats_attaques["xss"].append(attaque.attaque_xss(url))
+    print(attaque.resultats_attaques)
+
+
